ab53a73664affe16e24dab22e014bbc2c617d560
Unknown <rustam.galljamov@gmail.com>
2021-05-18 18:56:02 +0200

    CC: Delete unused scripts, assets and folders

--- scripts/config_light.py
+++ scripts/config_light.py
@@ -14,7 +14,7 @@ MAX_EPISODE_STEPS = 3000
 # TODO: remove COM reward, train longer with smaller LR decay, use exp clip_range sched
 # configure Weights & Biases
 WB_PROJECT_NAME = 'torch_cleanup'
-WB_EXPERIMENT_NAME = 'CC: deleted dim_reduct'
+WB_EXPERIMENT_NAME = 'CC: del assets'
 WB_EXPERIMENT_DESCRIPTION = 'Baseline before starting the cleanup experiments. ' \
                             '' \
                             'Use default entropy coefficient. ' \
