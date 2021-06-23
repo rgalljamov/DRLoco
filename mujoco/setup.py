fa80613e86deed563eeb251453e0fd72f257383b
Unknown <rustam.galljamov@gmail.com>
2021-06-23 12:17:16 +0300

    DOC: Fix gym_mimic_env installation by RTD

--- mujoco/setup.py
+++ mujoco/setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(name='gym_mimic_envs',
       version='0.5',
       # required packages for our env to work
-      install_requires=['gym', 'mujoco-py']
+      # uncommented to make ReadTheDocs build work
+      # install_requires=['gym', 'mujoco-py']
 )
+
