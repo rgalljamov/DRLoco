9f1843f2f1e1c6392d309c25eb52fd422dbb1e2b
Unknown <rustam.galljamov@gmail.com>
2021-07-19 00:00:28 +0200

    DOC: Start documenting the process of running an agent

--- docs/scripts/run.rst
+++ docs/scripts/run.rst
@@ -3,3 +3,25 @@
 
 Running an agent
 **********************
+
+How do we save agents?
+=======================
+
+During the training of an agent, we save multiple checkpoints:
+
+* initial agent: PPO with random networks directly after initialization and before training
+
+* final agent: PPO agent after the end of the training
+
+* agents reaching certain performance thresholds specified in $common/callback.py$.
+
+	* reaching 50, 60, ... 90% of maximum possible imitation reward
+
+	* reaching 50, 60, ... 90% of maximum possible episode return
+
+.. important:
+   
+   Each agent is always saved with a corresponding environment. This environment has to be loaded together with the agent to run the agent. This is done automatically within this framework. 
+
+   **Details:** We train our agents on mod:`VecNormalize` environment wrappers. These wrappers maintain a running mean and std of the observations (dimension-vise) to normalize the inputs into the policy and value function networks. Therefore, when loading an agent, we also have to wrap the corresponding environment in a VecNormalize object. This is done automatically within this framework. 
+   
--- drloco/run.py
+++ drloco/run.py
@@ -7,59 +7,62 @@ import sys
 from os import getcwd
 sys.path.append(getcwd())
 
-import gym, time, mujoco_py
 from drloco.mujoco.monitor_wrapper import Monitor
 from stable_baselines3 import PPO
 from drloco.common.utils import load_env, get_project_path
 from drloco.config import hypers as cfg
 from drloco.config import config as cfgl
-from drloco.mujoco.mimic_walker3d import MimicWalker3dEnv
-from drloco.mujoco.mimic_walker_165cm_65kg import MimicWalker165cm65kgEnv
 
-# paths
-# PD baseline
-path_pd_baseline = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/' \
-                   'cstm_pi/mim3d/8envs/ppo2/16mio/918-evaled-ret71'
-path_pd_normed_deltas = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/' \
-                        'pi_deltas/norm_acts/cstm_pi/mim3d/8envs/ppo2/16mio/431-evaled-ret81'
-path_trq_baseline = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/' \
-                    'cstm_pi/mim_trq_ff3d/8envs/ppo2/8mio/296-evaled-ret79'
-path_mirr_steps = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/' \
-                  'steps_mirr/cstm_pi/mim_trq_ff3d/8envs/ppo2/8mio/280'
-path_mirr_exps = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/' \
-                 'mirr_exps/cstm_pi/mim_trq_ff3d/8envs/ppo2/16mio/331-evaled-ret86'
-path_guoping = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/cstm_pi/' \
-               'mirr_exps/MimicWalker3d-v0/8envs/ppo2/8mio/361'
-path_140cm_40kg = '/mnt/88E4BD3EE4BD2EF6/Masters/M.Sc. Thesis/Code/models/dmm/cstm_pi/' \
-                  'refs_ramp/mirr_exps/MimicWalker3d-v0/8envs/ppo2/16mio/197-evaled-ret78'
-path_agent = get_project_path() + 'models/dmm/cstm_pi/mim_trq_ff3d/8envs/ppo2/8mio/296-evaled-ret79'
-path_agent = '/mnt/88E4BD3EE4BD2EF6/Users/Sony/Google Drive/WORK/DRL/CodeTorch/models/train/' \
-             'cstm_pi/refs_ramp/mirr_py/MimicWalker3d-v0/8envs/ppo2/4mio/885'
-
-DETERMINISTIC_ACTIONS = True
-RENDER = True
-
-SPEED_CONTROL = False
-speeds = [0.5, 1, 1.25, 1.25]
-duration_secs = 8
-
-PLAYBACK_TRAJECS = True
-
-# which model would you like to run
+# do you want to run a trained agent from a given path (FROM_PATH = True)
+# or do you want to run a random agent (can be used for testing purposes)
+#   ..important:
+#       If you load an agent from path, and the agent was trained using some modifications
+#       (variable 'modifications' in hypers.py was not an empty string,
+#       but e.g. MOD_MIRR_POLICY in hypers.py for policy mirroring), it is important,
+#       that the modifications variable is again the same as at the moment the agent was trained!
 FROM_PATH = False
+# absolute path to the trained agent
+#   ..important: just specify the path to the folder,
+#   where the model/ and env/ folders of your agent are
+path_agent = get_project_path() + 'models/dmm/cstm_pi/mim_trq_ff3d/8envs/ppo2/8mio/296-evaled-ret79'
 PATH = path_agent
+# we save multiple checkpoints during training. Which one should be used?
+# The name of the checkpoint is the text after 'model_' or 'env_'
+#   ..info: The agent saved at the end of the training has the name 'model_final'.
 checkpoint = 'final' # 'ep_ret2100_20M' # '33_min24mean24' # 'ep_ret2000_7M' #'mean_rew60'
 
-if FROM_PATH:
-    if not PATH.endswith('/'): PATH += '/'
 
-    # load model
+# should the agents behavior be rendered?
+RENDER = True
+# should the agent just play back the reference trajectories?
+# Can be used for testing the reference trajectories.
+PLAYBACK_TRAJECS = True
+
+# set to True, to apply a velocity profile as desired walking speed
+# during running the agent
+SPEED_CONTROL = False
+# specify the desired COM velocities on the profile in m/s
+# we'll linearly interpolate between these velocities
+# the velocities can be 1d (x-direction) or 2d: [(x1,y1), (x2,y2)],...]
+speeds = [0.5, 1, 1.25, 1.25]
+# how long should the desired walking speed profile be?
+duration_secs = 8
+
+assert not (PLAYBACK_TRAJECS and SPEED_CONTROL), \
+    'Controlling the COM speed while playing back trajectories is not supported!'
+
+
+if FROM_PATH and not PLAYBACK_TRAJECS:
+    # correct path if necessary
+    if not PATH.endswith('/'): PATH += '/'
+    # load the trained agent from path
     model_path = PATH + f'models/model_{checkpoint}'
     model = PPO.load(path=model_path)
-    print('\nModel:\n', model_path + '\n')
-
+    # load the corresponding environment from path
+    # necessary, to load the running mean and std of the observations.
     env = load_env(checkpoint, PATH, cfg.env_id)
 else:
+    # create a new environment as specified in the config.py
     from drloco.mujoco.config import env_map
     env = env_map[cfgl.ENV_ID]()
     env = Monitor(env)
@@ -69,27 +72,30 @@ else:
         env.activate_evaluation()
         env.playback_ref_trajectories(2000)
 
-if not isinstance(env, Monitor):
-    # VecNormalize wrapped DummyVecEnv
-    vec_env = env
-    env = env.venv.envs[0]
 
 if SPEED_CONTROL:
+    # set some flags and variables in the environment
+    # to have the agent follow a desired COM velocity profile
     env.activate_speed_control(speeds, duration_secs)
     cfg.ep_dur_max = duration_secs * cfgl.CTRL_FREQ
     des_speeds = []
     com_speeds = []
 
+# reset environment to get current state
 obs = vec_env.reset()
 # env.activate_evaluation()
 
-
+# run the agent for 10k steps in the environment
 for i in range(10000):
 
+    # get the actions and take a step in the environment
     if FROM_PATH:
-        action, hid_states = model.predict(obs, deterministic=DETERMINISTIC_ACTIONS)
+        # get actions from the loaded agent
+        action, hid_states = model.predict(obs, deterministic=True)
         obs, reward, done, _ = vec_env.step(action)
     else:
+        # get a random action, if no agent was loaded
+        # here, you can also specify your own custom actions (e.g. all zeros)
         action = env.action_space.sample()
         obs, reward, done, _ = env.step(action)
 
@@ -97,12 +103,15 @@ for i in range(10000):
     com_z_pos = env.get_COM_Z_position()
     done = com_z_pos < 0.5
 
+    # log desired and actual COM speeds if speed control is active
     if SPEED_CONTROL:
         des_speeds.append(env.desired_walking_speed)
         com_speeds.append(env.get_qvel()[0])
 
     if RENDER: env.render()
     if done: env.reset()
+
+    # in case speed control was active, compare the desired and actual velocities
     if SPEED_CONTROL and i >= cfg.ep_dur_max:
         from matplotlib import pyplot as plt
         plt.plot(des_speeds)
